16a8bde1944ae2b2f61fb6d5134937a4844985b7
pandit dandgule <panditdandgule777@gmail.com>
2022-02-24 08:33:23 +0530

    database connection.

--- employeedatabase/dbconnection.py
+++ employeedatabase/dbconnection.py
@@ -1,14 +1,22 @@
 import pymysql
 
-db_connection=pymysql.connect(host='localhost', user='root', password='root', port=3305, database='pythontest')
+db_connection=pymysql.connect(host='localhost',
+                              user='root',
+                              password='root',
+                              port=3305,
+                              database='')
 
 print(db_connection)
+
 # creating database_cursor to perform SQL operation
 db_cursor = db_connection.cursor()
+
 # executing cursor with execute method and pass SQL query
-db_cursor.execute("CREATE DATABASE my_first_db")
+db_cursor.execute("CREATE DATABASE my_db")
+
 # get list of all databases
 db_cursor.execute("SHOW DATABASES")
+
 #print all databases
 for db in db_cursor:
     print(db)
