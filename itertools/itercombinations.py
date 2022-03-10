ec738536f7fb9def844306d9091b958544a6ef29
pandit dandgule <panditdandgule777@gmail.com>
2022-03-10 09:30:59 +0530

    iter combinations.

--- itertools/itercombinations.py
+++ itertools/itercombinations.py
@@ -1,9 +1,8 @@
-import itertools
 from itertools import combinations
 
-help(itertools.combinations)
+#help(itertools.combinations)
 
-comb=itertools.combinations([9,10,2,1],2)
+comb=combinations([9,10,2,1],2)
 
 x=[i for i in comb]
 print(x)
