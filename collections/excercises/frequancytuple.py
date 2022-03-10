4b9d0db57d8dd471ad85bd2f8a54d0fe9224790a
pandit dandgule <panditdandgule777@gmail.com>
2022-03-10 08:26:15 +0530

    frequancy of tuple

---
+++ collections/excercises/frequancytuple.py
@@ -0,0 +1,6 @@
+from collections import Counter
+
+tup=[(['1', '4'], ['4', '1'], ['3', '4'], ['2', '7'], ['6', '8'], ['5', '8'], ['6', '8'], ['5', '7'], ['2', '7'])]
+
+for item in tup:
+    print(item)
